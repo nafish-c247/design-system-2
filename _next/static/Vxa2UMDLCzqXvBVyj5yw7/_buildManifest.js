664644f7052585cff96ff09f7fda92445dd1edda
nafish2102 <mo.nafish786@gmail.com>
2026-03-02 00:38:50 +0530

    Updates

--- _next/static/Vxa2UMDLCzqXvBVyj5yw7/_buildManifest.js
+++ _next/static/Vxa2UMDLCzqXvBVyj5yw7/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/design-system-2//_next/:path+",
+        "destination": "/design-system-2/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
