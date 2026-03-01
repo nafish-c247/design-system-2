ac74b7cd405075d09141bb2cfdeb3fe02be5cfcd
nafish2102 <mo.nafish786@gmail.com>
2026-03-02 00:55:10 +0530

    fix config changes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  reactCompiler: true,
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-
-  basePath: isProd ? "/design-system-2" : "",
-  assetPrefix: isProd ? "/design-system-2/" : "",
+  images: { unoptimized: true },
+  basePath: "/design-system-2",
+  assetPrefix: "/design-system-2/",
 };
 
 export default nextConfig;
